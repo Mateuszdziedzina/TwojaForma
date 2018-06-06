592ddfb629f3b57ba9f477ba396df4e3775106b9
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-06 22:44:43 +0200

    Local storage - show saved data
    - Add 'AppView.js' - module to manage application view
    - Add function to show saved personal info data
    - Use of local storage - 'getItem()'

---
+++ js/AppView.js
@@ -0,0 +1,16 @@
+﻿var AppView = (function(view) {
+
+    view.showSavedPersonalInfo = function(fieldToShow) {
+    	fieldToShow.html("<p> Moje zapisane dane: <br/>" +
+   	 			"Płeć: " + localStorage.getItem("myGender") + " <br/>" +
+   	 			"Waga: " + localStorage.getItem("myWeight") + " kg" + " <br/>" +
+   	 			"Wzrost: " + localStorage.getItem("myHeight") + " cm" + " <br/>" +
+   	 			"Wiek: " + localStorage.getItem("myAge") + " <br/>" +  	 	
+   	 			"Aktywność: " + localStorage.getItem("myActivityLevel") + "</p>"); 	 	
+    };
+    
+
+    return view;
+
+
+})(AppView || {});
