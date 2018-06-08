19facab1e8a7f52b79cb87b4f25da23db4074b46
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-08 19:26:11 +0200

    Take photo - success and fail functions
    - Add callback function to do action with taken photo - when photo is successfully taken
    - Add callback function to provide error message - when photo is not taken because of an error

--- js/PhotoManager.js
+++ js/PhotoManager.js
@@ -1,4 +1,16 @@
 
+function photoFileSuccess(imageData) {
+	var imageTaken = document.getElementById('photoTaken');
+	imageTaken.style.display = 'block';
+	imageTaken.src = imageData;
+}
+
+
+function photoFail(errorMessage) {
+	alert('Error: ' + errorMessage);
+}
+
+
 function takeAndAddPhoto() {
 	navigator.camera.getPicture(photoFileSuccess, photoFail, { quality: 80, destinationType: Camera.DestinationType.FILE_URI });
 }
