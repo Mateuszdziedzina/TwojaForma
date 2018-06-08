08e1f30185ba93df4d1246ec67606f8734e31310
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-08 21:59:30 +0200

    Get photo from selected source
    - Add function to get photo from selected source (for example album)
    - Add callback function to do action with selected photo - when photo is successfully get

--- js/PhotoManager.js
+++ js/PhotoManager.js
@@ -6,6 +6,13 @@ function photoFileSuccess(imageData) {
 }
 
 
+function photoURISuccess(imageURI) {
+	var imageFromLibrary = document.getElementById('photoFromLibrary');
+	imageFromLibrary.style.display = 'block';
+	imageFromLibrary.src = imageURI;
+}
+
+
 function photoFail(errorMessage) {
 	alert('Error: ' + errorMessage);
 }
@@ -13,4 +20,11 @@ function photoFail(errorMessage) {
 
 function takeAndAddPhoto() {
 	navigator.camera.getPicture(photoFileSuccess, photoFail, { quality: 80, destinationType: Camera.DestinationType.FILE_URI });
+}
+
+
+function getPhoto(source) {
+	navigator.camera.getPicture(photoURISuccess, photoFail, { quality: 80, 
+		        destinationType: destinationType.FILE_URI,
+		        sourceType: source });
 }
