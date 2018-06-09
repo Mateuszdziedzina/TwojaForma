060a14d8f4f1ac9a2df422ab273f17e7d41414e8
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-09 20:38:06 +0200

    Delete activity
    - Add function to delete activity from array which contains activities
    - Use function 'filter()'
    - Delete activity with selected ID
    - Other activities remain unchanged

--- js/ActivityManagement.js
+++ js/ActivityManagement.js
@@ -6,11 +6,23 @@ var ActivityManagement = (function(service) {
         return _activities;
     }
 
-
     service.addActivity = function(description, kcalPerHour) {
         _activities.push(new Activity(description, kcalPerHour));
-    }	
-	
+    }
+
+    service.deleteActivity = function(activityId) {
+        _activities = _activities.filter(function(activity) {
+            return activity.id !== activityId;
+        });
+    }
+
+    service.changeState = function(isCompleted, activityId) {
+        var activity = _activities.filter(function(activity) {
+            return activity.id === activityId;
+        })[0];
+
+        activity.isCompleted = isCompleted;
+    }
 
     return service;
 
