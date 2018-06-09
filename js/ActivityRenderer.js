dccd1d374e04c88d3a545d6e5c7e8f4ffe6e71ab
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-09 16:53:11 +0200

    Function to clear items in activity list
    - Add function to clear the activity list
    - Use 'removeChild()' function

--- js/ActivityRenderer.js
+++ js/ActivityRenderer.js
@@ -8,10 +8,17 @@
         if (!_listRoot) {
         	_listRoot = document.getElementById("activity-list-root");
         } else {
-
+            clearItems();
         }
     }	
 
+	
+    function clearItems() {
+        while (_listRoot.firstChild) {
+            _listRoot.removeChild(_listRoot.firstChild);
+        }
+    }	
+	
 	
     return renderer;
 
