1e9345d4458a531f454896879902148675318ad7
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-09 16:43:13 +0200

    Initialize root of the activity list
    - Add function to initialize root of the activity list
    - Check if list root is already set

--- js/ActivityRenderer.js
+++ js/ActivityRenderer.js
@@ -2,7 +2,17 @@
 
     var _listRoot = null;
 
-    
+	
+	
+    function initializeRoot() {
+        if (!_listRoot) {
+        	_listRoot = document.getElementById("activity-list-root");
+        } else {
+
+        }
+    }	
+
+	
     return renderer;
 
 })(ActivityRenderer || {});
