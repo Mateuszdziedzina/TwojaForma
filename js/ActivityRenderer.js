32e6f848f4180730f8825652aa625272b026fee2
Mateuszdziedzina <Mateuszdziedzina@users.noreply.github.com>
2018-06-10 10:41:28 +0200

    Template to create activity - layout
    - Collapsible field for activity
    - Checkbox - activity completed or not
    - Slider for select time of activity
    - Field to show calories which are burned for selected activity for selected time
    - Delete activity button

--- js/ActivityRenderer.js
+++ js/ActivityRenderer.js
@@ -37,6 +37,37 @@
 		
 		return `<div role="main">
 		
+					<div data-role="collapsible" data-content-theme="false">
+						<h2>Miejsce na opis</h2>
+					
+							<div>
+					    			<input data-role="flipswitch"name="flip-checkbox-3" id=""
+					    				data-on-text="Wykonano" data-off-text="Niewykonano"
+					    					data-wrapper-class="custom-size-flipswitch" type="checkbox">
+							</div>
+					
+					    <div>
+					        <div>
+								<label for=''>Czas wykonywania ćwiczenia (w minutach):</label>
+								<input name="slider-2" id='' data-highlight="true" min="0" max="100" value="60" type="range">
+					        </div>
+					
+							<a href="#checkCaloriesField" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all ui-shadow"
+								data-position-to="window" data-transition="pop"
+			
+								onclick="">
+									Sprawdź ilość spalonych kalorii</a>
+								<div data-role="popup" id="checkCaloriesField">					
+								</div>				                
+					    
+					        <div>
+					        	<button class="ui-btn ui-corner-all" id="add-activity-btn" onclick="">
+					        		Usuń
+					        	</button>
+					        </div>
+					    </div>
+					
+					</div>
 				</div>`  
     }
   	 
